e8beb8f7222b0ce348d01164b9ccb754c201d699
Little Green Man 1 <lgm1@pulsar.config>
2011-10-09 05:36:38 +0100

    More games

--- features.py
+++ features.py
@@ -10,19 +10,26 @@ def goodFeatures(img):
         eig_image = cv.CreateMat(img.rows, img.cols, cv.CV_32FC1)
         temp_image = cv.CreateMat(img.rows, img.cols, cv.CV_32FC1)
 	ret=[]
-        for (x,y) in cv.GoodFeaturesToTrack(img, eig_image, temp_image, 10, 0.04, 1.0, useHarris = True):
+        for (x,y) in cv.GoodFeaturesToTrack(img, eig_image, temp_image, 10000, 0.04, 1.0, useHarris = True):
 		ret.append((x,y))
 	return ret;
 
 #Takes file names
 def paintFeatures(inp,out):
+	#Open input image file
 	img = cv.LoadImageM(inp,cv.CV_LOAD_IMAGE_GRAYSCALE);
+	#get good features
 	gf=goodFeatures(img);
-	omg = cv.CreateImage(cv.GetSize(img), cv.IPL_DEPTH_16S, 3)
+	#create a copy og image
+	#matOut= cv.CreateMat(img.width,img.height,cv.IPL_DEPTH_16U)
+
+	omg =  cv.LoadImageM(inp,1);
+	#print "Omg:",omg.width,omg.height," ",omg.depth, "Img:",img.width,img.height," ",img.depth
+	#cv.Copy(img,omg)
 	for (x,y) in gf:
 		#print "x",int(x),"y",y
 		#print omg
-		cv.Circle(omg,(int(x),int(y)),20,(00,255,00))
+		cv.Circle(omg,(int(x),int(y)),5,(00,255,00))
 	cv.SaveImage(out, omg)
 
 	
@@ -35,7 +42,8 @@ def init():
 	print rgb_img.width
 def main():
 	global rgb_img;
-	init()
+	#init()
+	paintFeatures("distance-image.pgm","distance-image.features.png")
 	paintFeatures("rgb-image.ppm","rgb-image.features.png")
 #	gf=goodFeatures();
 	
